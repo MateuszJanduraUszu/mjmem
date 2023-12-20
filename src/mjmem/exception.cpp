544490a8ea6407467a629f2bfce282af0e308827
MateuszJanduraUszu <Mati4321@interia.pl>
2023-12-20 14:21:17 +0100

    Implement `allocation_limit_exceeded` and `resource_overrun` exceptions

--- src/mjmem/exception.cpp
+++ src/mjmem/exception.cpp
@@ -21,4 +21,20 @@ namespace mjx {
     void pool_exhausted::raise() {
         throw pool_exhausted();
     }
+
+    allocation_limit_exceeded::allocation_limit_exceeded() noexcept {}
+
+    allocation_limit_exceeded::~allocation_limit_exceeded() noexcept {}
+
+    void allocation_limit_exceeded::raise() {
+        throw allocation_limit_exceeded();
+    }
+
+    resource_overrun::resource_overrun() noexcept {}
+
+    resource_overrun::~resource_overrun() noexcept {}
+
+    void resource_overrun::raise() {
+        throw resource_overrun();
+    }
 } // namespace mjx
--- src/mjmem/exception.hpp
+++ src/mjmem/exception.hpp
@@ -26,6 +26,24 @@ namespace mjx {
         // raises pool exhausted exception
         __declspec(noreturn) static void raise();
     };
+
+    class _MJMEM_API allocation_limit_exceeded {
+    public:
+        allocation_limit_exceeded() noexcept;
+        ~allocation_limit_exceeded() noexcept;
+
+        // raises allocation limit exceeded exception
+        __declspec(noreturn) static void raise();
+    };
+
+    class _MJMEM_API resource_overrun {
+    public:
+        resource_overrun() noexcept;
+        ~resource_overrun() noexcept;
+
+        // raises resource overrun exception
+        __declspec(noreturn) static void raise();
+    };
 } // namespace mjx
 
 #endif // _MJMEM_EXCEPTION_HPP_
