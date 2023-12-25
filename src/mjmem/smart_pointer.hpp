90e10ca15e56dc2ccbad948cbf8b9da1bf426568
MateuszJanduraUszu <Mati4321@interia.pl>
2023-12-25 19:31:49 +0100

    `<smart_pointer>`: Rework and rename `unique_ptr` to `unique_smart_ptr` (#1)

--- src/mjmem/smart_pointer.hpp
+++ src/mjmem/smart_pointer.hpp
@@ -6,6 +6,7 @@
 #pragma once
 #ifndef _MJMEM_SMART_POINTER_HPP_
 #define _MJMEM_SMART_POINTER_HPP_
+#include <atomic>
 #include <mjmem/exception.hpp>
 #include <mjmem/object_allocator.hpp>
 #include <type_traits>
@@ -166,306 +167,105 @@ namespace mjx {
     }
 
     template <class _Ty>
-    struct default_pointer_deleter { // default deleter for any smart pointer
-        default_pointer_deleter() noexcept = default;
-
-        void operator()(_Ty* const _Ptr) const noexcept {
-            ::mjx::delete_object(_Ptr);
-        }
-    };
-
-    template <class _Ty>
-    struct default_array_deleter { // default deleter for array types in smart pointers
-        default_array_deleter() noexcept = default;
-
-        void operator()(_Ty* const _Ptr, const size_t _Size) const noexcept {
-            ::mjx::delete_objects(_Ptr, _Size);
-        }
-    };
-
-    template <class _Ty, class _Deleter = default_pointer_deleter<_Ty>>
-    class unique_ptr { // smart pointer with unique object ownership semantics
+    class unique_smart_ptr { // smart pointer with unique object ownership semantics
     public:
         using element_type = _Ty;
         using pointer      = _Ty*;
-        using deleter_type = _Deleter;
 
-        unique_ptr() noexcept : _Mypair() {}
+        unique_smart_ptr() noexcept : _Myptr(nullptr) {}
 
-        unique_ptr(nullptr_t) noexcept : _Mypair() {}
+        unique_smart_ptr(nullptr_t) noexcept : _Myptr(nullptr) {}
 
-        explicit unique_ptr(pointer _Ptr) noexcept : _Mypair(_Ptr, _Deleter{}) {}
+        explicit unique_smart_ptr(pointer _Ptr) noexcept : _Myptr(_Ptr) {}
 
-        unique_ptr(pointer _Ptr, const _Deleter& _Del) noexcept : _Mypair(_Ptr, _Del) {}
+        unique_smart_ptr(unique_smart_ptr&& _Other) noexcept : _Myptr(_Other.release()) {}
 
-        unique_ptr(pointer _Ptr, _Deleter&& _Del) noexcept : _Mypair(_Ptr, ::std::move(_Del)) {}
-
-        unique_ptr(unique_ptr&& _Other) noexcept
-            : _Mypair(_Other.release(), ::std::move(_Other._Mypair.second())) {}
-
-        ~unique_ptr() noexcept {
-            pointer& _Ptr = _Mypair.first();
-            if (_Ptr) {
-                _Mypair.second()(_Ptr);
-                _Ptr = nullptr;
+        ~unique_smart_ptr() noexcept {
+            if (_Myptr) {
+                ::mjx::delete_object(_Myptr);
+                _Myptr = nullptr;
             }
         }
 
-        unique_ptr& operator=(unique_ptr&& _Other) noexcept {
+        unique_smart_ptr& operator=(unique_smart_ptr&& _Other) noexcept {
             reset(_Other.release());
-            _Mypair.second() = ::std::forward<_Deleter>(_Other._Mypair.second());
             return *this;
         }
 
-        unique_ptr& operator=(nullptr_t) noexcept {
+        unique_smart_ptr& operator=(nullptr_t) noexcept {
             reset();
             return *this;
         }
 
-        unique_ptr(const unique_ptr&)            = delete;
-        unique_ptr& operator=(const unique_ptr&) = delete;
+        unique_smart_ptr(const unique_smart_ptr&)            = delete;
+        unique_smart_ptr& operator=(const unique_smart_ptr&) = delete;
 
-        explicit operator bool() const noexcept {
-            return _Mypair.first() != nullptr;
-        }
-
-        ::std::add_lvalue_reference_t<_Ty> operator*() const noexcept(noexcept(*::std::declval<pointer>())) {
-            return *_Mypair.first();
+        element_type& operator*() const noexcept(noexcept(*::std::declval<pointer>())) {
+            return *_Myptr;
         }
 
         pointer operator->() const noexcept {
-            return _Mypair.first();
+            return _Myptr;
+        }
+
+        explicit operator bool() const noexcept {
+            return _Myptr != nullptr;
         }
 
         pointer get() const noexcept {
-            return _Mypair.first();
-        }
-
-        deleter_type& get_deleter() noexcept {
-            return _Mypair.second();
-        }
-
-        const deleter_type& get_deleter() const noexcept {
-            return _Mypair.second();
+            return _Myptr;
         }
 
         pointer release() noexcept {
-            pointer& _Ptr    = _Mypair.first();
-            pointer _Old_ptr = _Ptr;
-            _Ptr             = nullptr;
+            pointer _Old_ptr = _Myptr;
+            _Myptr           = nullptr;
             return _Old_ptr;
         }
 
         void reset(pointer _New_ptr = nullptr) noexcept {
-            pointer& _Ptr = _Mypair.first();
-            if (_Ptr) {
-                _Mypair.second()(_Ptr);
+            if (_Myptr) {
+                ::mjx::delete_object(_Myptr);
             }
 
-            _Ptr = _New_ptr;
+            _Myptr = _New_ptr;
         }
 
-        void swap(unique_ptr& _Other) noexcept {
-            _Mypair.swap(_Other._Mypair);
+        void swap(unique_smart_ptr& _Other) noexcept {
+            ::std::swap(_Myptr, _Other._Myptr);
         }
 
     private:
-        optimized_pair<pointer, _Deleter> _Mypair;
+        pointer _Myptr;
     };
 
-    template <class _Ty, class _Deleter>
-    inline bool operator==(const unique_ptr<_Ty, _Deleter>& _Left, const unique_ptr<_Ty, _Deleter>& _Right) {
+    template <class _Ty>
+    inline bool operator==(const unique_smart_ptr<_Ty>& _Left, const unique_smart_ptr<_Ty>& _Right) noexcept {
         return _Left.get() == _Right.get();
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator==(const unique_ptr<_Ty, _Deleter>& _Unique, nullptr_t) {
-        return !_Unique;
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator==(nullptr_t, const unique_ptr<_Ty, _Deleter>& _Unique) {
-        return !_Unique;
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(const unique_ptr<_Ty, _Deleter>& _Left, const unique_ptr<_Ty, _Deleter>& _Right) {
-        return _Left.get() != _Right.get();
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(const unique_ptr<_Ty, _Deleter>& _Unique, nullptr_t) {
-        return static_cast<bool>(_Unique);
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(nullptr_t, const unique_ptr<_Ty, _Deleter>& _Unique) {
-        return static_cast<bool>(_Unique);
-    }
-
-    template <class _Ty, class _Deleter = default_array_deleter<_Ty>>
-    class unique_array { // exclusive ownership smart pointer for arrays
-    public:
-        using element_type = _Ty;
-        using pointer      = _Ty*;
-        using deleter_type = _Deleter;
-
-        struct array {
-            pointer ptr = nullptr;
-            size_t size = 0;
-        };
-
-        unique_array() noexcept : _Mypair() {}
-
-        unique_array(nullptr_t) noexcept : _Mypair() {}
-
-        unique_array(pointer _Ptr, const size_t _Size) noexcept : _Mypair(array{_Ptr, _Size}, _Deleter{}) {}
-
-        unique_array(pointer _Ptr, const size_t _Size, const _Deleter& _Del) noexcept
-            : _Mypair(array{_Ptr, _Size}, _Del) {}
-
-        unique_array(pointer _Ptr, const size_t _Size, _Deleter&& _Del) noexcept
-            : _Mypair(array{_Ptr, _Size}, ::std::move(_Del)) {}
-
-        unique_array(unique_array&& _Other) noexcept
-            : _Mypair(_Other.release(), ::std::move(_Other._Mypair.second())) {}
-
-        ~unique_array() noexcept {
-            array& _Array = _Mypair.first();
-            if (_Array.ptr) {
-                _Mypair.second()(_Array.ptr, _Array.size);
-                _Array.ptr  = nullptr;
-                _Array.size = 0;
-            }
-        }
-
-        unique_array& operator=(unique_array&& _Other) noexcept {
-            const array& _Array = _Other.release();
-            reset(_Array.ptr, _Array.size);
-            _Mypair.second() = ::std::forward<_Deleter>(_Other._Mypair.second());
-            return *this;
-        }
-
-        unique_array& operator=(nullptr_t) noexcept {
-            reset();
-            return *this;
-        }
-
-        unique_array(const unique_array&)            = delete;
-        unique_array& operator=(const unique_array&) = delete;
-
-        explicit operator bool() const noexcept {
-            return _Mypair.first().ptr != nullptr;
-        }
-
-        ::std::add_lvalue_reference_t<_Ty> operator*() const noexcept(noexcept(*::std::declval<pointer>())) {
-            return *_Mypair.first().ptr;
-        }
-
-        pointer operator->() const noexcept {
-            return _Mypair.first().ptr;
-        }
-
-        _Ty& operator[](const size_t _Idx) const {
-            const array& _Array = _Mypair.first();
-            if (_Idx >= _Array.size) {
-                resource_overrun::raise();
-            }
-
-            return _Array.ptr[_Idx];
-        }
-
-        pointer get() const noexcept {
-            return _Mypair.first().ptr;
-        }
-
-        size_t size() const noexcept {
-            return _Mypair.first().size;
-        }
-
-        deleter_type& get_deleter() noexcept {
-            return _Mypair.second();
-        }
-
-        const deleter_type& get_deleter() const noexcept {
-            return _Mypair.second();
-        }
-
-        array release() noexcept {
-            array& _Array    = _Mypair.first();
-            array _Old_array = {_Array.ptr, _Array.size};
-            _Array.ptr       = nullptr;
-            _Array.size      = 0;
-            return _Old_array;
-        }
-
-        void reset(pointer _New_ptr, const size_t _New_size) noexcept {
-            array& _Array = _Mypair.first();
-            if (_Array.ptr) {
-                _Mypair.second()(_Array.ptr, _Array.size);
-            }
-
-            _Array.ptr  = _New_ptr;
-            _Array.size = _New_size;
-        }
-
-        void reset(nullptr_t = nullptr) noexcept {
-            array& _Array = _Mypair.first();
-            if (_Array.ptr) {
-                _Mypair.second()(_Array.ptr, _Array.size);
-                _Array.ptr  = nullptr;
-                _Array.size = 0;
-            }
-        }
-
-        void swap(unique_array& _Other) noexcept {
-            _Mypair.swap(_Other._Mypair);
-        }
-
-    private:
-        optimized_pair<array, _Deleter> _Mypair;
-    };
-
-    template <class _Ty, class _Deleter>
-    inline bool operator==(
-        const unique_array<_Ty, _Deleter>& _Left, const unique_array<_Ty, _Deleter>& _Right) {
-        return _Left.get() == _Right.get();
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator==(const unique_array<_Ty, _Deleter>& _Unique, nullptr_t) {
-        return !_Unique;
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator==(nullptr_t, const unique_array<_Ty, _Deleter>& _Unique) {
-        return !_Unique;
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(
-        const unique_array<_Ty, _Deleter>& _Left, const unique_array<_Ty, _Deleter>& _Right) {
-        return _Left.get() != _Right.get();
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(const unique_array<_Ty, _Deleter>& _Unique, nullptr_t) {
-        return static_cast<bool>(_Unique);
-    }
-
-    template <class _Ty, class _Deleter>
-    inline bool operator!=(nullptr_t, const unique_array<_Ty, _Deleter>& _Unique) {
-        return static_cast<bool>(_Unique);
-    }
-
-    template <class _Ty, class... _Types>
-    inline unique_ptr<_Ty> make_unique_ptr(_Types&&... _Args) {
-        return unique_ptr<_Ty>(::mjx::create_object<_Ty>(::std::forward<_Types>(_Args)...));
     }
 
     template <class _Ty>
-    inline unique_array<_Ty> make_unique_array(const size_t _Size) {
-        return unique_array<_Ty>(::mjx::allocate_objects<_Ty>(_Size), _Size);
+    inline bool operator==(const unique_smart_ptr<_Ty>& _Left, nullptr_t) noexcept {
+        return !_Left;
+    }
+
+    template <class _Ty>
+    inline bool operator==(nullptr_t, const unique_smart_ptr<_Ty>& _Right) noexcept {
+        return !_Right;
+    }
+
+    template <class _Ty>
+    inline bool operator!=(const unique_smart_ptr<_Ty>& _Left, const unique_smart_ptr<_Ty>& _Right) noexcept {
+        return _Left.get() != _Right.get();
+    }
+
+    template <class _Ty>
+    inline bool operator!=(const unique_smart_ptr<_Ty>& _Left, nullptr_t) noexcept {
+        return static_cast<bool>(_Left);
+    }
+
+    template <class _Ty>
+    inline bool operator!=(nullptr_t, const unique_smart_ptr<_Ty>& _Right) noexcept {
+        return static_cast<bool>(_Right);
     }
 } // namespace mjx
 
