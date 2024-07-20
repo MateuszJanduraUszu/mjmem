a81ba6063638783c93937b19165c81bdf2983de2
Mateusz Jandura <mjandura03@gmail.com>
2024-07-20 16:05:10 +0200

    `<block_allocator>`: Optimize internal bitmap (#49)

--- src/mjmem/block_allocator.cpp
+++ src/mjmem/block_allocator.cpp
@@ -7,8 +7,9 @@
 #include <mjmem/block_allocator.hpp>
 #include <mjmem/dynamic_allocator.hpp>
 #include <mjmem/exception.hpp>
+#include <mjmem/impl/bitops.hpp>
 #include <mjmem/impl/utils.hpp>
-#include <utility>
+#include <mjmem/object_allocator.hpp>
 
 namespace mjx {
     block_allocator::block_allocator(pool_resource& _Resource, const size_type _Block_size)
@@ -20,7 +21,7 @@ namespace mjx {
         //       which will fail the assertion, expecting the remainder to be 0.
         _INTERNAL_ASSERT(_Resource.size() % _Block_size == 0, "resource size must be a multiple of block size");
 #endif // _DEBUG
-        _Setup_bitmap();
+        _Init_bitmap();
     }
 
     block_allocator::~block_allocator() noexcept {}
@@ -32,7 +33,7 @@ namespace mjx {
     block_allocator::_Bitmap::~_Bitmap() noexcept {
         if (_Size > _Small_buffer_size) { // deallocate larger buffer
             dynamic_allocator _Al;
-            _Al.deallocate(_Ptr, _Size * sizeof(_Word_type));
+            ::mjx::delete_object_array_using_allocator(_Ptr, _Size, _Al);
         }
     }
 
@@ -44,156 +45,45 @@ namespace mjx {
         return _Size <= _Small_buffer_size ? _Buf : _Ptr;
     }
 
-    block_allocator::size_type block_allocator::_Bitmap::_Bits_per_last_word(const size_type _Max) const noexcept {
-        return _Bits_per_word - ((_Size * _Bits_per_word) % _Max);
-    }
-
-    block_allocator::size_type block_allocator::_Bitmap::_Find_zero_bits(
-        const size_type _Count, const size_type _Max) const noexcept {
-        const _Word_type* const _Words = _Get_words();
-        size_type _Bits                = _Bits_per_word;
-        size_type _Zeros               = 0; // number of zero bits
-        size_type _Off; // number of bit
-        for (size_type _Word = 0; _Word < _Size; ++_Word) {
-            if (_Words[_Word] == _All_bits_set) { // all bits set, skip this word
-                continue;
-            }
-
-            if (_Word == _Size - 1) { // last word may use less bits
-                _Bits = _Bits_per_last_word(_Max);
-            }
-
-            for (size_type _Bit = 0; _Bit < _Bits; ++_Bit) {
-                if ((_Words[_Word] & (1 << _Bit)) == 0) { // found zero bit
-                    if (_Zeros++ == 0) { // set start of this occurence
-                        _Off = (_Word * _Bits_per_word) + _Bit;
-                    }
-
-                    if (_Zeros == _Count) { // found requested number of zero bits, break
-                        return _Off;
-                    }
-                } else {
-                    _Zeros = 0; // reset counter
-                }
-            }
-        }
-
-        return static_cast<size_type>(-1); // not found
-    }
-
-    void block_allocator::_Bitmap::_Set_bits(size_type _Off, size_type _Count) noexcept {
-        _Word_type* const _Words       = _Get_words();
-        size_type _Word                = _Off / _Bits_per_word;
-        size_type _Word_remaining_bits = (::std::min)(_Count, _Bits_per_word - (_Off % _Bits_per_word));
-        size_type _Shift               = _Off % _Bits_per_word;
-        _Word_type _Mask;
-        for (;;) {
-            if (_Word_remaining_bits == _Bits_per_word) { // set all bits
-                _Mask = _All_bits_set;
-            } else { // set n bits
-                _Mask = static_cast<_Word_type>(((1 << _Word_remaining_bits) - 1) << _Shift);
-            }
-
-            _Words[_Word] |= _Mask;
-            _Count        -= _Word_remaining_bits;
-            if (_Count == 0) { // no more bits to set
-                break;
-            }
-
-            ++_Word; // advance to the next word
-            _Shift               = 0; // start from the first bit, no shifting
-            _Word_remaining_bits = (::std::min)(_Bits_per_word, _Count);
-        }
-    }
-
-    void block_allocator::_Bitmap::_Zero_bits(size_type _Off, size_type _Count) noexcept {
-        _Word_type* const _Words       = _Get_words();
-        size_type _Word                = _Off / _Bits_per_word;
-        size_type _Word_remaining_bits = (::std::min)(_Count, _Bits_per_word - (_Off % _Bits_per_word));
-        size_type _Shift               = _Off % _Bits_per_word;
-        _Word_type _Mask;
-        for (;;) {
-            if (_Word_remaining_bits == _Bits_per_word) { // zero all bits
-                _Mask = _All_bits_set;
-            } else { // zero n bits
-                _Mask = static_cast<_Word_type>(((1 << _Word_remaining_bits) - 1) << _Shift);
-            }
-
-            _Words[_Word] &= ~_Mask;
-            _Count        -= _Word_remaining_bits;
-            if (_Count == 0) { // no more bits to set
-                break;
-            }
-
-            ++_Word; // advance to the next word
-            _Shift               = 0; // start from the first bit, no shifting
-            _Word_remaining_bits = (::std::min)(_Bits_per_word, _Count);
-        }
-    }
-
-    void block_allocator::_Setup_bitmap() {
+    void block_allocator::_Init_bitmap() {
         using _Word_type       = _Bitmap::_Word_type;
-        const size_type _Bits  = _Max_blocks(); // required number of bits
-        const size_type _Words = (_Bits + _Bitmap::_Bits_per_word - 1) / _Bitmap::_Bits_per_word;
+        using _Traits          = mjmem_impl::_Word_traits<_Word_type>;
+        const size_type _Bits  = _Total_block_count(); // required number of bits
+        const size_type _Words = (_Bits + _Traits::_Bits_per_word - 1) / _Traits::_Bits_per_word;
         if (_Words > _Bitmap::_Small_buffer_size) { // use larger buffer
             dynamic_allocator _Al;
-            _Mymap._Ptr = static_cast<_Word_type*>(_Al.allocate(_Words * sizeof(_Word_type)));
-            ::memset(_Mymap._Ptr, 0, _Words * sizeof(_Word_type)); // make sure each bit stores 0
+            const size_type _Bytes = _Words * sizeof(_Word_type); // number of bytes to allocate
+            _Mymap._Ptr            = static_cast<_Word_type*>(_Al.allocate(_Bytes));
+            ::memset(_Mymap._Ptr, 0, _Bytes); // make sure each bit is set to 0
         }
         
         _Mymap._Size = _Words;
         _Mymap._Free = _Bits;
     }
 
-    block_allocator::size_type block_allocator::_Required_block_count(const size_type _Bytes) const noexcept {
-        return mjmem_impl::_Align_value(_Bytes, _Myblock) / _Myblock;
+    block_allocator::size_type block_allocator::_Least_block_count(const size_type _Bytes) const noexcept {
+        // calculate the least number of blocks that will fit the given number of bytes
+        return (_Bytes + _Myblock - 1) / _Myblock;
     }
 
-    block_allocator::size_type block_allocator::_Max_blocks() const noexcept {
+    block_allocator::size_type block_allocator::_Total_block_count() const noexcept {
+        // calculate the total number of fixed-size blocks
         return _Myres.size() / _Myblock;
     }
 
-    block_allocator::pointer block_allocator::_Allocate_block() noexcept {
-        if (_Mymap._Free == 0) { // no free blocks
-            return nullptr;
-        }
-
-        using _Word_type         = _Bitmap::_Word_type;
-        _Word_type* const _Words = _Mymap._Get_words();
-        size_type _Bits          = _Bitmap::_Bits_per_word;
-        for (size_type _Word = 0; _Word < _Mymap._Size; ++_Word) {
-            if (_Words[_Word] == _Bitmap::_All_bits_set) { // definitely no free block
-                continue;
-            }
-
-            if (_Word == _Mymap._Size - 1) { // last word may use less bits
-                _Bits = _Mymap._Bits_per_last_word(_Max_blocks());
-            }
-
-            for (size_type _Bit = 0; _Bit < _Bitmap::_Bits_per_word; ++_Bit) {
-                if ((_Words[_Word] & (1 << _Bit)) == 0) { // found free block, use it
-                    _Words[_Word] |= 1 << _Bit; // mark as used
-                    return static_cast<pointer>(static_cast<unsigned char*>(_Myres.data())
-                        + (((_Word * _Bitmap::_Bits_per_word) + _Bit) * _Myblock));
-                }
-            }
-        }
-
-        return nullptr;
-    }
-
-    block_allocator::pointer block_allocator::_Allocate_n_blocks(const size_type _Count) noexcept {
+    block_allocator::pointer block_allocator::_Allocate_blocks(const size_type _Count) noexcept {
         if (_Mymap._Free < _Count) { // not enough free blocks
             return nullptr;
         }
         
-        const size_type _Off = _Mymap._Find_zero_bits(_Count, _Max_blocks());
-        if (_Off == static_cast<size_type>(-1)) { // not enough space, break
+        mjmem_impl::_Word_array _Array(_Mymap._Get_words(), _Mymap._Size, _Total_block_count());
+        const size_type _Off = mjmem_impl::_Find_contiguous_zero_bits(_Array, _Count);
+        if (_Off == static_cast<size_type>(-1)) { // not enough free space, break
             return nullptr;
         }
 
-        _Mymap._Set_bits(_Off, _Count);
-        return static_cast<pointer>(static_cast<unsigned char*>(_Myres.data()) + (_Off * _Myblock));
+        mjmem_impl::_Set_bit_range(_Array, _Off, _Count); // mark the allocated blocks as used
+        return mjmem_impl::_Adjust_address_by_offset(_Myres.data(), _Off * _Myblock);
     }
 
     block_allocator::pointer block_allocator::allocate(const size_type _Count) {
@@ -201,9 +91,9 @@ namespace mjx {
             return nullptr;
         }
 
-        const size_type _Blocks = _Required_block_count(_Count);
-        pointer _Ptr            = _Blocks == 1 ? _Allocate_block() : _Allocate_n_blocks(_Blocks);
-        if (!_Ptr) { // not enough memory, raise an exception
+        const size_type _Blocks = _Least_block_count(_Count);
+        pointer _Ptr            = _Allocate_blocks(_Blocks);
+        if (!_Ptr) { // not enough free space, raise an exception
 #if _MJMEM_VERSION_SUPPORTED(1, 0, 1)
             allocation_failure::raise();
 #else // ^^^ _MJMEM_VERSION_SUPPORTED(1, 0, 1) ^^^ / vvv !_MJMEM_VERSION_SUPPORTED(1, 0, 1) vvv
@@ -211,7 +101,7 @@ namespace mjx {
 #endif // _MJMEM_VERSION_SUPPORTED(1, 0, 1)
         }
 
-        _Mymap._Free -= _Blocks;
+        _Mymap._Free -= _Blocks; // update the number of free blocks
         return _Ptr;
     }
 
@@ -224,11 +114,12 @@ namespace mjx {
 
     void block_allocator::deallocate(pointer _Ptr, const size_type _Count) noexcept {
         if (_Ptr && _Count > 0) {
-            const size_type _Blocks = _Required_block_count(_Count);
+            const size_type _Blocks = _Least_block_count(_Count);
             if (_Myres.contains(_Ptr, _Blocks * _Myblock)) { // _Ptr allocated from the associated resource
-                _Mymap._Zero_bits((static_cast<unsigned char*>(_Ptr)
-                    - static_cast<unsigned char*>(_Myres.data())) / _Myblock, _Blocks);
-                _Mymap._Free += _Blocks;
+                mjmem_impl::_Word_array _Array(_Mymap._Get_words(), _Mymap._Size);
+                mjmem_impl::_Clear_bit_range(
+                    _Array, mjmem_impl::_Calculate_offset_from_address(_Myres.data(), _Ptr) / _Myblock, _Blocks);
+                _Mymap._Free += _Blocks; // update the number of free blocks
             }
         }
     }
---
+++ src/mjmem/impl/bitops.hpp
@@ -0,0 +1,307 @@
+// bitops.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJMEM_IMPL_BITOPS_HPP_
+#define _MJMEM_IMPL_BITOPS_HPP_
+#include <bit>
+#include <climits>
+#include <cstdint>
+#include <utility>
+
+namespace mjx {
+    namespace mjmem_impl {
+        template <class _Ty>
+        constexpr const _Ty& _Min(const _Ty& _Val0, const _Ty& _Val1, const _Ty& _Val2) noexcept {
+            // return the smallest number among the three elements
+            return (::std::min)(_Val0, (::std::min)(_Val1, _Val2));
+        }
+
+        template <class _Word_type>
+        struct _Word_traits { // general word-dependent traits
+            static constexpr size_t _Bits_per_word     = sizeof(_Word_type) * CHAR_BIT;
+            static constexpr size_t _Not_found         = static_cast<size_t>(-1);
+            static constexpr _Word_type _All_bits_set  = ~_Word_type{0};
+            static constexpr _Word_type _None_bits_set = _Word_type{0};
+        };
+
+        using _Base_word_type   = uint8_t;
+        using _Base_word_traits = _Word_traits<_Base_word_type>;
+
+        template <class _Word_type>
+        constexpr size_t _Find_zero_bit(const _Word_type _Word) noexcept {
+            // search for the first zero bit in a word, -1 if not found
+            using _Traits     = _Word_traits<_Word_type>;
+            const size_t _Off = static_cast<size_t>(::std::countr_one(_Word));
+            return _Off < _Traits::_Bits_per_word ? _Off : _Traits::_Not_found;
+        }
+
+        template <class _Word_type>
+        constexpr size_t _Count_trailing_zeros(const _Word_type _Word) noexcept {
+            // count the number of trailing zeros in a word
+            return static_cast<size_t>(::std::countr_zero(_Word));
+        }
+
+        template <class _Word_type>
+        constexpr size_t _Find_contiguous_zero_bits_in_word(const _Word_type _Word, const size_t _Count) noexcept {
+            // search for a contiguous sequence of zero bits of length _Count in the given _Word 
+            using _Traits = _Word_traits<_Word_type>;
+            switch (_Count) {
+            case 0: // searching for zero bits always succeeds
+                return 0;
+            case 1: // search for exactly one zero bit
+                return _Find_zero_bit(_Word);
+            case _Traits::_Bits_per_word: // search for all bits in the word
+                return _Word == _Traits::_None_bits_set ? 0 : _Traits::_Not_found;
+            default:
+                if (_Count > _Traits::_Bits_per_word) { // more bits requested than available in the word, break
+                    return _Traits::_Not_found;
+                }
+
+                break;
+            }
+
+            size_t _Shift = 0; // shift used to discard already checked bits
+            size_t _Off; // offset of the sequence occurrence
+            size_t _Found; // number of zero bits found in the current sequence
+            size_t _Remaining = _Traits::_Bits_per_word; // number of bits available in the word
+            for (; _Shift < _Traits::_Bits_per_word; _Shift = _Off + 1) {
+                // Note: Since we already checked if _Count is equal to 1, we can break
+                //       if the zero bit was found in the last bit, as it means there is only
+                //       one zero bit in the sequence.
+                _Off = _Find_zero_bit(static_cast<_Word_type>(_Word >> _Shift));
+                if (_Off == _Traits::_Not_found || _Off == _Traits::_Bits_per_word - 1) {
+                    // no more zero bits or only one zero bit left
+                    break;
+                }
+
+                _Off      += _Shift; // add shift to make offset absolute
+                _Found     = _Count_trailing_zeros(static_cast<_Word_type>(_Word >> (_Off + 1))) + 1;
+                _Remaining = _Traits::_Bits_per_word - _Off;
+
+                // Note: Due to shifting, there are always bits that don't belong to _Word,
+                //       thus we need to check if the found number of bits is within the word.
+                if (_Found >= _Count && _Remaining >= _Count) { // the given number of bits found, break
+                    return _Off;
+                }
+            }
+
+            return _Traits::_Not_found;
+        }
+
+        struct _Word_array { // stores an array of 1-byte words
+            using _Word_type = _Base_word_type;
+
+            constexpr _Word_array(_Word_type* const _Words, const size_t _Size, const size_t _Bits = 0) noexcept
+                : _Words(_Words), _Size(_Size),
+                _Bits(_Bits != 0 ? _Bits : _Size * _Base_word_traits::_Bits_per_word) {}
+
+            _Word_type* _Words;
+            size_t _Size; // the number of words
+            size_t _Bits; // total number of bits (used in _Find_contiguous_zero_bits-related functions)
+        };
+
+        struct _Contiguous_zero_bits_search_state {
+            size_t _Off   = _Base_word_traits::_Not_found; // offset of a sequence of zero bits
+            size_t _Found = 0; // total number of zero bits found
+            size_t _Idx   = 0; // index of the current word
+        };
+
+        template <class _Word_type>
+        constexpr size_t _Calculate_bits_per_last_word(const size_t _Bits) noexcept {
+            // return the number of bits in the last word, which may be incomplete
+            using _Traits           = _Word_traits<_Word_type>;
+            const size_t _Remainder = _Bits % _Base_word_traits::_Bits_per_word;
+            if (_Remainder == 0) { // word is complete, return all bits
+                return _Traits::_Bits_per_word;
+            }
+
+            // the last word has incomplete bits, calculate how many bits are present
+            constexpr size_t _Full_words = sizeof(_Word_type) - 1;
+            return (_Full_words * _Base_word_traits::_Bits_per_word) + _Remainder;
+        }
+
+        template <class _Word_type>
+        constexpr bool _Find_contiguous_zero_bits_step(
+            const _Word_array& _Array, const size_t _Count, _Contiguous_zero_bits_search_state& _State) noexcept {
+            // search for a contiguous sequence of zero bits in the _Array
+            using _Traits                  = _Word_traits<_Word_type>;
+            const size_t _Bits_per_word    = _State._Idx + sizeof(_Word_type) == _Array._Size
+                ? _Calculate_bits_per_last_word<_Word_type>(_Array._Bits) : _Traits::_Bits_per_word;
+            const _Word_type _Word         = *reinterpret_cast<const _Word_type*>(_Array._Words + _State._Idx);
+            const _Word_type _All_bits_set = _Traits::_All_bits_set >> (_Traits::_Bits_per_word - _Bits_per_word);
+            size_t _Off;
+            size_t _Search;
+            if (_Word == _Traits::_None_bits_set) { // all bits set to zero, count them and break
+                if (_State._Off == _Traits::_Not_found) { // capture the start of the occurrence
+                    _State._Off = _State._Idx * _Base_word_traits::_Bits_per_word;
+                }
+
+                _State._Found += _Bits_per_word;
+                return _State._Found >= _Count;
+            } else if (_Word == _All_bits_set) { // no zero bits found in this word, reset the counter
+                _State._Off   = _Traits::_Not_found;
+                _State._Found = 0;
+                return false;
+            }
+
+            if (_State._Off != _Traits::_Not_found) { // continue searching from the previous word
+                _Search = (::std::min)(_Count - _State._Found, _Bits_per_word);
+                _Off    = _Find_contiguous_zero_bits_in_word(_Word, _Search);
+                if (_Off != 0) { // sequence not contiguous, reset the counter
+                    _State._Off   = _Traits::_Not_found;
+                    _State._Found = 0;
+                } else { // sequence contiguous, update the counter
+                    _State._Found += _Search;
+                    return _State._Found >= _Count;
+                }
+            }
+
+            size_t _Shift     = 0; // shift used to discard already checked bits
+            size_t _Remaining = _Bits_per_word; // number of bits available in the word
+            size_t _Found; // number of zero bits found in the current sequence
+            for (; _Shift < _Bits_per_word; _Shift = _Off + 1) {
+                _Off = _Find_zero_bit(static_cast<_Word_type>(_Word >> _Shift));
+                if (_Off == _Traits::_Not_found || _Off >= _Bits_per_word) { // no more zero bits in the word, break
+                    break;
+                }
+
+                _Off      += _Shift; // add shift to make offset absolute
+                _Search    = _Min(_Bits_per_word - _Off, _Bits_per_word, _Count);
+                _Found     = _Count_trailing_zeros(static_cast<_Word_type>(_Word >> (_Off + 1))) + 1;
+                _Remaining = _Bits_per_word - _Off;
+                if (_Found >= _Search && _Remaining >= _Search) { // the given amount of zero bits found, break
+                    _State._Off   = (_State._Idx * _Base_word_traits::_Bits_per_word) + _Off;
+                    _State._Found = (::std::min)(_Found, _Remaining);
+                    return _State._Found >= _Count;
+                }
+            }
+
+            return false;
+        }
+
+        using _Contiguous_zero_bits_search_step_callback = bool(*)(
+            const _Word_array&, const size_t, _Contiguous_zero_bits_search_state&) noexcept;
+
+        struct _Contiguous_zero_bits_search_step {
+            _Contiguous_zero_bits_search_step_callback _Callback;
+            size_t _Count; // the number of words processed by _Callback
+
+            static constexpr _Contiguous_zero_bits_search_step _Choose(const size_t _Remaining) noexcept {
+                // choose the appropriate callback and step size based on the number of remaining words
+                if (_Remaining >= 8) {
+                    return {&_Find_contiguous_zero_bits_step<uint64_t>, 8};
+                } else if (_Remaining >= 4) {
+                    return {&_Find_contiguous_zero_bits_step<uint32_t>, 4};
+                } else if (_Remaining >= 2) {
+                    return {&_Find_contiguous_zero_bits_step<uint16_t>, 2};
+                } else {
+                    return {&_Find_contiguous_zero_bits_step<uint8_t>, 1};
+                }
+            }
+        };
+
+        constexpr size_t _Find_contiguous_zero_bits(const _Word_array& _Array, const size_t _Count) noexcept {
+            // search for a contiguous sequence of zero bits of length _Count in the given _Array
+            _Contiguous_zero_bits_search_state _State;
+            _Contiguous_zero_bits_search_step _Step;
+            for (; _State._Idx < _Array._Size; _State._Idx += _Step._Count) {
+                _Step = _Contiguous_zero_bits_search_step::_Choose(_Array._Size - _State._Idx);
+                if (_Step._Callback(_Array, _Count, _State)) { // the given number of bits found, break
+                    return _State._Off;
+                }
+            }
+
+            return _Base_word_traits::_Not_found;
+        }
+
+        struct _Bit_range_modification_state {
+            size_t _Off       = _Base_word_traits::_Not_found; // offset of the range
+            size_t _Idx       = _Base_word_traits::_Not_found; // index of the current word
+            size_t _Remaining = 0; // the number of bits remaining to be modified
+            bool _Set         = false; // true if setting bits to 1, false if setting bits to 0
+        };
+
+        template <class _Word_type>
+        constexpr void _Modify_bit_range_step(_Word_array& _Array, _Bit_range_modification_state& _State) noexcept {
+            // modify a range of bits in the _Array based on the current state
+            using _Traits = _Word_traits<_Word_type>;
+            size_t _Off;
+            size_t _Count;
+            if (_State._Idx == _Traits::_Not_found) { // first word
+                _State._Idx = _State._Off / _Base_word_traits::_Bits_per_word;
+                _Off        = _State._Off % _Base_word_traits::_Bits_per_word == 0
+                    ? 0 : _State._Off % _Base_word_traits::_Bits_per_word;
+                _Count      = (::std::min)(_State._Remaining, _Traits::_Bits_per_word - _Off);
+            } else { // subsequent words
+                _Off   = 0; // start from the beginning of the word
+                _Count = (::std::min)(_State._Remaining, _Traits::_Bits_per_word);
+            }
+
+            _Word_type& _Word = *reinterpret_cast<_Word_type*>(_Array._Words + _State._Idx);
+            _Word_type _Mask  = 0;
+            if (_Count == _Traits::_Bits_per_word) { // modify all bits in the word
+                _Mask = _Traits::_All_bits_set;
+            } else { // modify only a portion of the bits
+                _Mask = static_cast<_Word_type>(((1 << _Count) - 1) << _Off);
+            }
+
+            if (_State._Set) { // set bits to 1
+                _Word |= _Mask;
+            } else { // set bits to 0
+                _Word &= ~_Mask;
+            }
+
+            _State._Remaining -= _Count; // update the number of remaining bits to modify
+        }
+
+        using _Bit_range_modification_step_callback = void(*)(
+            _Word_array&, _Bit_range_modification_state&) noexcept;
+
+        struct _Bit_range_modification_step {
+            _Bit_range_modification_step_callback _Callback;
+            size_t _Count; // the number of words processed by _Callback
+        
+            static constexpr _Bit_range_modification_step _Choose(const size_t _Remaining) noexcept {
+                // choose the appropriate callback and step size based on the number of remaining words
+                if (_Remaining >= 64) {
+                    return {&_Modify_bit_range_step<uint64_t>, 8};
+                } else if (_Remaining >= 32) {
+                    return {&_Modify_bit_range_step<uint32_t>, 4};
+                } else if (_Remaining >= 16) {
+                    return {&_Modify_bit_range_step<uint16_t>, 2};
+                } else {
+                    return {&_Modify_bit_range_step<uint8_t>, 1};
+                }
+            }
+        };
+
+        constexpr void _Modify_bit_range(
+            _Word_array& _Array, const size_t _Off, const size_t _Count, const bool _Set) noexcept {
+            // modify bits in the range [_Off, _Off + _Count) to either 0 or 1 based on the _Set flag
+            _Bit_range_modification_state _State;
+            _State._Off       = _Off;
+            _State._Remaining = _Count;
+            _State._Set       = _Set;
+            _Bit_range_modification_step _Step;
+            for (; _State._Remaining > 0; _State._Idx += _Step._Count) {
+                _Step = _Bit_range_modification_step::_Choose(_State._Remaining);
+                _Step._Callback(_Array, _State);
+            }
+        }
+
+        constexpr void _Set_bit_range(_Word_array& _Array, const size_t _Off, const size_t _Count) noexcept {
+            // set all bits to 1 in the range [_Off, _Off + _Count)
+            _Modify_bit_range(_Array, _Off, _Count, true);
+        }
+
+        constexpr void _Clear_bit_range(_Word_array& _Array, const size_t _Off, const size_t _Count) noexcept {
+            // set all bits to 0 in the range [_Off, _Off + _Count)
+            _Modify_bit_range(_Array, _Off, _Count, false);
+        }
+    } // namespace mjmem_impl
+} // namespace mjx
+
+#endif // _MJMEM_IMPL_BITOPS_HPP_
